35c66cf655b0b2855476d40c062b274eda9553b2
Мурашко Егор <murashkoegor@yandex.ru>
2021-10-30 14:22:42 +0300

    Add IsLeapYear func in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include "time.h"
+
+class DateTime
+{
+public:
+  int year;
+  int month;
+  int day;
+  int hour;
+  int min;
+  int sec;
+  int millisec;
+};
+
+bool IsLeapYear(DateTime dt)
+{
+	return (dt.year %4 == 0 && dt.year %100 != 0) || (dt.year %400 == 0);
+}
 
 int main(){
 
