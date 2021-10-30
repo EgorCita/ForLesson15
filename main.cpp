ed57fb3d62b63ee1764b148d26ecafca32396fb9
Мурашко Егор <murashkoegor@yandex.ru>
2021-10-30 14:34:38 +0300

    Add LaterInYear func and change IsLeapYear func main.cpp

--- main.cpp
+++ main.cpp
@@ -13,9 +13,21 @@ public:
   int millisec;
 };
 
-bool IsLeapYear(DateTime dt)
+bool IsLeapYear(DateTime & dt)
 {
 	return (dt.year %4 == 0 && dt.year %100 != 0) || (dt.year %400 == 0);
+}
+
+int LaterInYear(DateTime & dt1, DateTime & dt2)
+{
+	if ((dt1.month == dt2.month ? dt1.day > dt2.day : dt1.month > dt2.month))
+	{
+		return 1;
+	}
+	else
+	{
+		return 2;
+	}
 }
 
 int main(){
